39c9485fa5cd8450233e8ed63ef0a15b274f406c
dan <53418645+drpott@users.noreply.github.com>
2023-01-06 12:58:20 +1100

    Update helloWorldSimple.groovy

--- vars/helloWorldSimple.groovy
+++ vars/helloWorldSimple.groovy
@@ -1,3 +1,20 @@
-def call(String name, String dayOfWeek) {
-  sh "echo Hello World ${name}. It is ${dayOfWeek}."
+def call(String message) {
+    RED='\033[0;31m'
+    LIGHT_RED='\033[1;31m'
+    BLUE='\033[0;34m'
+    LIGHT_BLUE='\033[1;34m'
+    CYAN='\033[0;36m'
+    LIGHT_CYAN='\033[1;36m'
+    GREEN='\033[0;32m'
+    LIGHT_GREEN='\033[1;32m'
+    PURPLE='\033[0;35m'
+    LIGHT_PURPLE='\033[1;35m'
+    BROWN='\033[0;33m'
+    ON_YELLOW='\033[0;43m'
+    GRAY='\033[0;30m'
+    LIGHT_GRAY='\033[0;37m'
+    NC='\033[0m'
+    ansiColor('xterm') {
+      echo "${BLUE} Hello World ${message}. ${NC}"
+    }
 }
